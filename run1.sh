668fd022098d3a4b6a644076a9cbaa805e3bd445
swansoat5662 <ats5662@psu.edu>
2023-03-05 10:51:47 -0500

    fix errors

--- run1.sh
+++ run1.sh
@@ -4,8 +4,8 @@ cp ~/SchwarzP_Lidinoid_GFIT_Aeroacoustic_Simulation/ImpedanceDataNIT/SchwarzP_PL
 cp ~/SchwarzP_Lidinoid_GFIT_Aeroacoustic_Simulation/ImpedanceDataNIT/SchwarzP_PLA_Resistance.txt .
 cp ~/SchwarzP_Lidinoid_GFIT_Aeroacoustic_Simulation/Material/mat.xml .
 cp ~/SchwarzP_Lidinoid_GFIT_Aeroacoustic_Simulation/Mesh/GFITMesh.cdb .
-cfs -t2 SchwarzP_GFIT_M00_PLA
-cfs -t2 SchwarzP_GFIT_M075_PLA
-cfs -t2 SchwarzP_GFIT_M05_PLA
-cfs -t2 SchwarzP_GFIT_M025_PLA
+cfs -t2 SchwarzP_GFIT_M00_PLA -d
+cfs -t2 SchwarzP_GFIT_M075_PLA -d
+cfs -t2 SchwarzP_GFIT_M05_PLA -d
+cfs -t2 SchwarzP_GFIT_M025_PLA -d
 rm SchwarzP_PLA_Reactance.txt SchwarzP_PLA_Resistance.txt mat.xml GFITMesh.cdb 
--- run2.sh
+++ run2.sh
@@ -4,8 +4,8 @@ cp ~/SchwarzP_Lidinoid_GFIT_Aeroacoustic_Simulation/ImpedanceDataNIT/SchwarzP_TP
 cp ~/SchwarzP_Lidinoid_GFIT_Aeroacoustic_Simulation/ImpedanceDataNIT/SchwarzP_TPU_Resistance.txt .
 cp ~/SchwarzP_Lidinoid_GFIT_Aeroacoustic_Simulation/Material/mat.xml .
 cp ~/SchwarzP_Lidinoid_GFIT_Aeroacoustic_Simulation/Mesh/GFITMesh.cdb .
-cfs -t2 SchwarzP_GFIT_M00_TPU
-cfs -t2 SchwarzP_GFIT_M075_TPU
-cfs -t2 SchwarzP_GFIT_M05_TPU
-cfs -t2 SchwarzP_GFIT_M025_TPU
+cfs -t2 SchwarzP_GFIT_M00_TPU -d
+cfs -t2 SchwarzP_GFIT_M075_TPU -d
+cfs -t2 SchwarzP_GFIT_M05_TPU -d
+cfs -t2 SchwarzP_GFIT_M025_TPU -d
 rm SchwarzP_TPU_Reactance.txt SchwarzP_TPU_Resistance.txt mat.xml GFITMesh.cdb 
